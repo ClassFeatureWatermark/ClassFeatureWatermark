76d6a98ed04cef71d8ac5019fc815be4e8bd03a0
ClassFeatureWatermark <removalresisitant@163.com>
2025-01-13 17:08:21 +0800

    Update imagenet.py

--- dataset/imagenet.py
+++ dataset/imagenet.py
@@ -9,52 +9,6 @@ import pickle
 
 from torchvision.transforms import transforms
 
-
-# class ImageNet(Dataset):
-#     def __init__(self, root, train=False, original_transforms=None):
-#         self.data = []
-#         self.labels = []
-#         self.transform = transforms.Compose([
-#             original_transforms,
-#             transforms.Resize((32, 32))])
-#         self.train = train
-#         self.load_data(root)
-#
-#     def load_data(self, root):
-#         # 加载所有批次文件
-#         if self.train:
-#             for i in range(1, 11):  # 从1到10
-#                 print("i", i)
-#                 file_path = f"{root}/Imagenet64_train/train_data_batch_{i}"
-#                 with open(file_path, 'rb') as file:
-#                     batch_data = pickle.load(file, encoding='bytes')
-#                 # ImageNet64 数据通常是 N x 3072 的数组，需要重新塑形为 64x64x3
-#                 images = batch_data['data'].reshape((-1, 3, 64, 64)).transpose((0, 2, 3, 1))
-#                 self.data.extend(images)
-#                 self.labels.extend(batch_data['labels'])
-#         else:
-#             file_path = f"{root}/Imagenet64_val/val_data"
-#             with open(file_path, 'rb') as file:
-#                 batch_data = pickle.load(file, encoding='bytes')
-#             # ImageNet64 数据通常是 N x 3072 的数组，需要重新塑形为 64x64x3
-#             images = batch_data['data'].reshape((-1, 3, 64, 64)).transpose((0, 2, 3, 1))
-#             self.data.extend(images)
-#             self.labels.extend(batch_data['labels'])
-#
-#     def __len__(self):
-#         return len(self.data)
-#
-#     def __getitem__(self, idx):
-#         img = self.data[idx]
-#         label = self.labels[idx] - 1  # 标签调整为从0开始
-#         img = Image.fromarray(img)
-#
-#         if self.transform:
-#             img = self.transform(img)
-#
-#         return img, label
-
-
 class Imagenet64(Dataset):
     def __init__(self, root, transforms, train=False):
         self.data = []
@@ -66,13 +20,11 @@ class Imagenet64(Dataset):
         self.label_mapping = {old_label: new_label for new_label, old_label in enumerate(self.unique_labels)}
         # print(self.label_mapping)
     def load_data(self, root):
-        # 加载所有批次文件
         if self.train:
-            for i in range(1, 11):  # 从1到10
+            for i in range(1, 11):
                 file_path = f"{root}/Imagenet64_train/train_data_batch_{i}"
                 with open(file_path, 'rb') as file:
                     batch_data = pickle.load(file, encoding='bytes')
-                # ImageNet64 数据通常是 N x 3072 的数组，需要重新塑形为 64x64x3
                 images = batch_data['data'].reshape((-1, 3, 64, 64)).transpose((0, 2, 3, 1))
                 self.data.extend(images)
                 self.labels.extend(batch_data['labels'])
@@ -80,7 +32,6 @@ class Imagenet64(Dataset):
             file_path = f"{root}/Imagenet64_val/val_data"
             with open(file_path, 'rb') as file:
                 batch_data = pickle.load(file, encoding='bytes')
-            # ImageNet64 数据通常是 N x 3072 的数组，需要重新塑形为 64x64x3
             images = batch_data['data'].reshape((-1, 3, 64, 64)).transpose((0, 2, 3, 1))
             self.data.extend(images)
             self.labels.extend(batch_data['labels'])
